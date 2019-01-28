25e83c9ed5abdcb6d25e12877427af3d366bf988
= <kirillsumovskyi@gmail.com>
2019-01-28 11:05:30 +0200

    project architecture change

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './pages/home/home.jsx'
+import SignIn from './pages/sign-in/sign-in.jsx'
 import Forgot from './pages/forgot/forgot.jsx'
 import {browserHistory, Router, Route} from 'react-router'
 
@@ -10,5 +11,5 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path='/' component={Home} />
     <Route path='/forgot' component={Forgot} />
-    <Route path='/singup' component={Home} />
+    <Route path='/sign-in' component={SignIn} />
   </Router>, document.getElementById('root'))
--- app/pages/home/home.jsx
+++ app/pages/home/home.jsx
@@ -1,45 +1,15 @@
-import React, {Component} from 'react'
-import {backgroundImage} from 'services'
+import React from 'react'
+import SignIn from '../sign-in/sign-in.jsx'
+import Forgot from '../forgot/forgot.jsx'
+
 import './home.less'
 
-let mail = 'mail.svg'
-let lock = 'lock.svg'
-
-class Home extends Component {
+class Home extends React.Component {
   render () {
     return (
-      <div style={{backgroundImage: `url(${_config.urls.static}coffee-cup-laptop-and-notepad.png)`}} className='sign-in'>
-        <div className='sign-in-wrap'>
-          <img className='sign-in-htm__logo' src={_config.urls.static + 'logo.svg'} />
-          <div className='login-form'>
-            <div className='login-form__text'>{_config.translations.title}</div>
-            <button className='login-form__button'>
-              <img className='login-form__img' src={_config.urls.static + 'search.svg'} />
-              <span>{_config.translations.login_google}</span>
-            </button>
-            <span className='login-form__text or'>{_config.translations.login_or}</span>
-            <div className='group'>
-              <input type='text'
-                style={{background: backgroundImage(mail)}}
-                className='group__input'
-                placeholder={_config.translations.enter_email} />
-            </div>
-            <div className='group'>
-              <input type='password'
-                style={{background: backgroundImage(lock)}}
-                className='group__input'
-                data-type='password'
-                placeholder={_config.translations.enter_password} />
-            </div>
-            <button className='login-form__button login-button'>
-              <span>{_config.translations.login}</span>
-            </button>
-            <span className='login-form__forgot'>{_config.translations.forgot_password}</span>
-          </div>
-        </div>
-        <span className='dont-have-acc'>{_config.translations.dont_have_acc} &nbsp;
-          <span className='dont-have-acc__sing-up'>{_config.translations.sing_up}</span>
-        </span>
+      <div id='home'>
+        <SignIn home />
+        <Forgot />
       </div>
     )
   }
