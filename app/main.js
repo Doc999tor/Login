18348c4af0a889cbb0d0aeffdeb88647a9e96e23
schvvarz <kirillsumovskyi@gmail.com>
2019-02-11 09:50:26 +0200

    react router (old)

--- app/main.js
+++ app/main.js
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SignIn from './pages/sign-in/sign-in.jsx'
 import Forgot from './pages/forgot/forgot.jsx'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {browserHistory, Router, Route} from 'react-router'
 
 import './style.less'
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path={_config.routing.sing_in_path} component={SignIn} />
-      <Route path={_config.routing.forgot_path} component={Forgot} />
-    </Switch>
+  <Router history={browserHistory}>
+    <Route path={_config.routing.sing_in_path} component={SignIn} />
+    <Route path={_config.routing.forgot_path} component={Forgot} />
   </Router>, document.getElementById('root'))
