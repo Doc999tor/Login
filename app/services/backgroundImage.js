57945227f73df47f83577bbde465aa8e20dea8f3
schvvarz <kirillsumovskyi@gmail.com>
2019-02-21 15:31:11 +0200

    fix background img position (rotate)

--- app/services/backgroundImage.js
+++ app/services/backgroundImage.js
@@ -1,4 +1,4 @@
 const backgroundImage = imgName => {
-  return `url(${_config.urls.static}${imgName}) no-repeat ${window._config.data.isRTL ? '93% 50%' : '7% 50%'}`
+  return `url(${_config.urls.static}${imgName}) no-repeat ${window._config.data.isRTL ? 'right 1rem center' : 'left 1rem center'}`
 }
 export default backgroundImage
