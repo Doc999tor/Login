dfc23af8850acdcd4e061fbbf09530a98361996e
schvvarz <kirillsumovskyi@gmail.com>
2019-02-21 19:28:33 +0200

    redirect to signin page

--- app/main.js
+++ app/main.js
@@ -11,5 +11,6 @@ ReactDOM.render(
     <Switch>
       <Route exact path={_config.routing.sign_in_path} component={SignIn} />
       <Route path={_config.routing.forgot_path} component={Forgot} />
+      <Route path='*' component={SignIn} />
     </Switch>
   </Router>, document.getElementById('root'))
