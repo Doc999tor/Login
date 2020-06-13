861e89faaa203dd76fe243504ccbd3714acfa997
tkachuks <serhiytkachuk88@gmail.com>
2020-06-13 09:22:45 +0300

    upd npm packages and webpack.config for new version of Login module

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SignIn from './pages/sign-in/sign-in.jsx'
-import Forgot from './pages/forgot/forgot.jsx'
+// import Forgot from './pages/forgot/forgot.jsx'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './style.less'
@@ -10,7 +10,7 @@ ReactDOM.render(
   <Router>
     <Switch>
       <Route exact path={_config.routing.sign_in_path} component={SignIn} />
-      <Route path={_config.routing.forgot_path} component={Forgot} />
+      {/* <Route path={_config.routing.forgot_path} component={Forgot} /> */}
       <Route path='*' component={SignIn} />
     </Switch>
   </Router>, document.getElementById('root'))
